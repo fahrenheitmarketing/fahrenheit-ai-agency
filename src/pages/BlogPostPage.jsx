c25d01c76482d532f8808b7f1fc04b3e77521347
Ricardo Casas <rcasas@fahrenheitmarketing.com>
2026-04-21 03:22:19 +0000

    File changes

--- src/pages/BlogPostPage.jsx
+++ src/pages/BlogPostPage.jsx
@@ -93,8 +93,8 @@ export default function BlogPostPage() {
             <ReactMarkdown className="prose prose-lg max-w-none font-body
               prose-headings:font-heading prose-headings:font-normal prose-headings:text-accent
               prose-h2:text-2xl md:prose-h2:text-3xl prose-h2:mt-20 prose-h2:mb-10
-              prose-h3:text-xl md:prose-h3:text-2xl prose-h3:mt-16 prose-h3:mb-7 prose-h3:text-accent
-              prose-h4:text-lg md:prose-h4:text-xl prose-h4:mt-12 prose-h4:mb-6 prose-h4:text-accent
+              prose-h3:text-xl md:prose-h3:text-2xl prose-h3:mt-16 prose-h3:mb-7 prose-h3:!text-accent
+              prose-h4:text-lg md:prose-h4:text-xl prose-h4:mt-12 prose-h4:mb-6 prose-h4:!text-accent
               prose-p:text-base prose-p:leading-loose prose-p:mb-9
               prose-a:text-accent prose-a:underline
               prose-img:rounded-sm prose-img:my-12
