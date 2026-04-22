59590dbc3869575819f697e275f92b423be5b23b
Ricardo Casas <rcasas@fahrenheitmarketing.com>
2026-04-22 14:07:54 +0000

    File changes

--- src/components/home/ClientMarquee.jsx
+++ src/components/home/ClientMarquee.jsx
@@ -31,7 +31,7 @@ const clients = [
   {
     name: 'Sentech',
     logo: 'https://media.base44.com/images/public/69e6c4bd9bbd15c86a9a4b38/91cfda926_image.png',
-    scale: 1.5,
+    scale: 1.95,
   },
   {
     name: 'Cactus Life Sciences',
