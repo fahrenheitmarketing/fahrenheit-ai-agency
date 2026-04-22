bf5f6eecbf9a425450a011851408d9469047e161
Ricardo Casas <rcasas@fahrenheitmarketing.com>
2026-04-22 00:26:19 +0000

    File changes

--- src/components/home/ClientMarquee.jsx
+++ src/components/home/ClientMarquee.jsx
@@ -31,7 +31,7 @@ const clients = [
   },
   {
     name: 'Cactus Life Sciences',
-    logo: null,
+    logo: 'https://media.base44.com/images/public/69e6c4bd9bbd15c86a9a4b38/afa88c46a_image.png',
   },
   {
     name: 'Lammes',
